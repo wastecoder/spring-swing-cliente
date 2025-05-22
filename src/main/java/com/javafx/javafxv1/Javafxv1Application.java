d52ce99486514a191fa0e79db7764c489c96d46a
Paulino <paulinomendes03@gmail.com>
2025-05-22 18:30:19 -0300

    feat: remove arquivos desnecessários

--- src/main/java/com/javafx/javafxv1/Javafxv1Application.java
+++ src/main/java/com/javafx/javafxv1/Javafxv1Application.java
@@ -2,8 +2,6 @@ package com.javafx.javafxv1;
 
 import com.javafx.javafxv1.config.AppConfig;
 import com.javafx.javafxv1.ui.ClienteForm;
-import com.javafx.javafxv1.ui.MainWindow;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
